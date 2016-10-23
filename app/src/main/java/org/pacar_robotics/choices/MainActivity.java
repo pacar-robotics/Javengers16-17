e2ee32d839e5ed5f2ac7b6db457b4c76b1f33934
Rahul Ramkumar <rahulram226@gmail.com>
2016-10-23 18:16:12 -0400

    Add Method Stub for Saving when bnt_save is Pressed
    Will have to change method this later to include file saving

--- app/src/main/java/org/pacar_robotics/choices/MainActivity.java
+++ app/src/main/java/org/pacar_robotics/choices/MainActivity.java
@@ -1,13 +1,22 @@
 package org.pacar_robotics.choices;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+	@OnClick(R.id.btn_save)
+	void save() {
+		// TODO: Save xml or json file
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ButterKnife.bind(this);
 	}
 }
