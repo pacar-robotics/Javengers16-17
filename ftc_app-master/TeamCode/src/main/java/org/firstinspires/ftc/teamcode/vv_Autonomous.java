13ac3ec25813f4c3320de8c0785338645d6320e7
rachaelchacko <rachaelchacko@yahoo.com>
2016-10-23 17:38:21 -0400

    Created vv_Autonomous class
    Created vv_Autonomous class and added method stubs for moveTillLine()
    and turnCumulative()

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vv_Autonomous.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Rachael_ on 10/23/2016.
+ */
+
+public class vv_Autonomous extends vv_OpMode {
+
+    vv_Lib vvLib;
+    public void runOpMode() throws InterruptedException {
+        vvLib = new vv_Lib(this);
+    }
+
+    public void turnCumulative() throws InterruptedException {
+        //using gyro sensor
+        //stub
+    }
+
+    public void moveTillLine() throws InterruptedException {
+        //stub
+    }
+
+
+}
