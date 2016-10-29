e3e66851173a96b4c8320717f5622b30e6ec1ee9
Rahul Ramkumar <rahulram226@gmail.com>
2016-10-29 14:52:18 -0400

    Change EditText to SeekBar to Reflect Delay Value
    When SeekBar is changed, TextView next to it changes to current progress

--- app/src/main/java/org/pacar_robotics/choices/MainActivity.java
+++ app/src/main/java/org/pacar_robotics/choices/MainActivity.java
@@ -2,11 +2,17 @@ package org.pacar_robotics.choices;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+	@BindView(R.id.seek_delay)	SeekBar delaySeek;
+	@BindView(R.id.text_delay) TextView delayText;
 
 	@OnClick(R.id.btn_save)
 	void save() {
@@ -18,5 +24,19 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		ButterKnife.bind(this);
+
+		delaySeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				// Update the delay text view to reflect current choice
+				delayText.setText(Integer.toString(progress));
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {}
+		});
 	}
 }
