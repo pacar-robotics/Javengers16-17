1f256f020ab1ba41389b6f4ec717b13230d8e0b9
Rahul Ramkumar <rahulram226@gmail.com>
2016-10-30 17:23:36 -0400

    Get App to Create File based on Current Choices

--- app/src/main/java/org/pacar_robotics/choices/MainActivity.java
+++ app/src/main/java/org/pacar_robotics/choices/MainActivity.java
@@ -4,13 +4,33 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,9 +39,19 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
 	private static final int MY_PERMISSION_REQUEST_STORAGE = 101;
+	private static final String LOG_TAG = "MainActivity";
+	private static final String FILE = Environment.getExternalStorageDirectory().getPath() +
+			"/PACAR/AutoChoices.xml";
 
 	@BindView(R.id.seek_delay) SeekBar delaySeek;
 	@BindView(R.id.text_delay) TextView delayText;
+	@BindView(R.id.radio_group_alliance) RadioGroup allianceGroup;
+	@BindView(R.id.radio_group_starting_position) RadioGroup startingPositionGroup;
+	@BindView(R.id.beacon_1) CheckBox beacon1Check;
+	@BindView(R.id.beacon_2) CheckBox beacon2Check;
+	@BindView(R.id.center_vortex) CheckBox centerVortexCheck;
+	@BindView(R.id.corner_vortex) CheckBox cornerVortexCheck;
+	@BindView(R.id.block) CheckBox blockCheck;
 
 	@OnClick(R.id.btn_save)
 	void saveButtonClicked() {
@@ -33,9 +63,35 @@ public class MainActivity extends AppCompatActivity {
 					MY_PERMISSION_REQUEST_STORAGE);
 		} else {
 			// Permission is granted, move on
-			Toast.makeText(this, "Permission has been granted", Toast.LENGTH_SHORT).show();
-			//TODO: create and save file
+			Log.v(LOG_TAG, "External Storage Permission granted");
+			writeXML(createHashMap());
+			Log.v(LOG_TAG, "Finished writeXML function");
 		}
+	}
+
+	private LinkedHashMap<String, String> createHashMap() {
+		LinkedHashMap<String, String> choicesMap = new LinkedHashMap<>();
+
+		// We have to remove spaces between the tags or else it will crash
+		// Alliance
+		RadioButton allianceButton = (RadioButton) findViewById(allianceGroup.getCheckedRadioButtonId());
+		choicesMap.put(getString(R.string.alliance_header).replace(" ", ""), allianceButton.getText().toString());
+
+		// Starting position
+		RadioButton startingPositionButton = (RadioButton) findViewById(startingPositionGroup.getCheckedRadioButtonId());
+		choicesMap.put(getString(R.string.starting_position_header).replace(" ", ""), startingPositionButton.getText().toString());
+
+		// Checkbox Choices
+		choicesMap.put(getString(R.string.beacon_1).replace(" ", ""), Boolean.toString(beacon1Check.isChecked()));
+		choicesMap.put(getString(R.string.beacon_2).replace(" ", ""), Boolean.toString(beacon2Check.isChecked()));
+		choicesMap.put(getString(R.string.park_on_center_vortex_partially).replace(" ", ""), Boolean.toString(centerVortexCheck.isChecked()));
+		choicesMap.put(getString(R.string.park_on_corner_vortex).replace(" ", ""), Boolean.toString(cornerVortexCheck.isChecked()));
+		choicesMap.put(getString(R.string.block_opposing_team).replace(" ", ""), Boolean.toString(blockCheck.isChecked()));
+
+		// Delay
+		choicesMap.put(getString(R.string.delay_header).replace(" ", ""), Integer.toString(delaySeek.getProgress()));
+
+		return choicesMap;
 	}
 
 	@Override
@@ -63,6 +119,7 @@ public class MainActivity extends AppCompatActivity {
 	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
 	                                       @NonNull int[] grantResults) {
 		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
 		switch (requestCode) {
 			case MY_PERMISSION_REQUEST_STORAGE:
 				if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -88,8 +145,52 @@ public class MainActivity extends AppCompatActivity {
 			default:
 				// There is no way the program can get to this statement
 				// If it does, something is very, very wrong
+				Log.e(LOG_TAG, "Reached default statement of onRequestPermissionResult");
 				Toast.makeText(this, "Something bad happened", Toast.LENGTH_SHORT).show();
 				break;
+		}
+	}
+
+	private void writeXML(LinkedHashMap<String, String> choicesMap) {
+		File file = new File(FILE);
+
+		if (file.delete()) {
+			Log.v(LOG_TAG, " File deleted:" + FILE);
+		} else {
+			Log.v(LOG_TAG, " File NOT deleted:" + FILE);
+		}
+
+		try {
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+			// Create root elements
+			Document doc = docBuilder.newDocument();
+			Element root = doc.createElement("AutoChoices");
+			doc.appendChild(root);
+
+			for (Map.Entry<String, String> entry : choicesMap.entrySet()) {
+				Element i = doc.createElement(entry.getKey());
+				i.appendChild(doc.createTextNode(entry.getValue()));
+				root.appendChild(i);
+			}
+
+			// Write the content into xml file
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+
+			DOMSource source = new DOMSource(doc);
+
+			StreamResult result = new StreamResult(file);
+
+			// Fix XML formatting
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+
+			transformer.transform(source, result);
+		} catch (Exception e) {
+			Toast.makeText(this, "Oh no! File creation failed!", Toast.LENGTH_LONG).show();
+			Log.e(LOG_TAG, "File creation failed: " + e.getMessage());
 		}
 	}
 }
