72e17352bdd1fb70743f4b7119fe6ba4b5c0b48b
Rahul Ramkumar <rahulram226@gmail.com>
2016-11-20 15:35:24 -0500

    Create New DiagnosticsFragment
    Delete old default fragments for tab layout

--- app/src/main/java/org/pacar_robotics/choicest/MainActivity.java
+++ app/src/main/java/org/pacar_robotics/choicest/MainActivity.java
@@ -1,23 +1,15 @@
 package org.pacar_robotics.choicest;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-import android.widget.TextView;
+import layout.DiagnosticsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -56,41 +48,6 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		private static final String ARG_SECTION_NUMBER = "section_number";
-
-		public PlaceholderFragment() {
-		}
-
-		/**
-		 * Returns a new instance of this fragment for the given section
-		 * number.
-		 */
-		public static PlaceholderFragment newInstance(int sectionNumber) {
-			PlaceholderFragment fragment = new PlaceholderFragment();
-			Bundle args = new Bundle();
-			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-		                         Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-			TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-			textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-			return rootView;
-		}
-	}
-
-	/**
 	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
 	 * one of the sections/tabs/pages.
 	 */
@@ -103,11 +60,14 @@ public class MainActivity extends AppCompatActivity {
 		@Override
 		public Fragment getItem(int position) {
 			// getItem is called to instantiate the fragment for the given page.\
-
-			if (position == 0) {
-				return new AutonomousFragment();
+			switch (position) {
+				case 0:
+					return new AutonomousFragment();
+				case 1:
+					return new DiagnosticsFragment();
+				default:
+					return null;
 			}
-			return PlaceholderFragment.newInstance(position + 1);
 		}
 
 		@Override
@@ -123,8 +83,9 @@ public class MainActivity extends AppCompatActivity {
 					return "AUTONOMOUS";
 				case 1:
 					return "DIAGNOSTICS";
+				default:
+					return null;
 			}
-			return null;
 		}
 	}
 }
