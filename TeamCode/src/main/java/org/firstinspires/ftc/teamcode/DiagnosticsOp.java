af5499d50eb219e4057066e2295802a2dff5341a
Rahul Ramkumar <rahulram226@gmail.com>
2016-11-27 20:44:04 -0500

    Create DiagnosticsOp.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiagnosticsOp.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Rahul on 11/27/2016.
+ */
+
+public class DiagnosticsOp extends vv_OpMode {
+
+	@Override
+	public void runOpMode() throws InterruptedException {
+		
+	}
+}
