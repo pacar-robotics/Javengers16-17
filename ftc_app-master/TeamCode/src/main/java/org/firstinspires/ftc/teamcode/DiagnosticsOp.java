02ffde7dd6444c089ad4d7ad328871cc2003e950
Jordan Shopp <jordan.shopp123@gmail.com>
2017-09-24 17:17:37 -0400

    Add Method Stubs for Tests
    WIll replace stubs with actual methods once common classses are developed

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiagnosticsOp.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiagnosticsOp.java
@@ -13,6 +13,32 @@ public class DiagnosticsOp extends vv_OpMode {
         robotLibrary = new vv_Lib(this);
     }
 
+
+    // TODO: Create actual tests once common class methods are completed
+    private boolean vuforiaDetection() {
+        return true;
+    }
+
+    private boolean cubeArm() {
+        return true;
+    }
+
+    private boolean cubeHand() {
+        return true;
+    }
+
+    private boolean cubeHandGyro() {
+        return true;
+    }
+
+    private boolean relicArm() {
+        return true;
+    }
+
+    private boolean relicHand() {
+        return true;
+    }
+
     // Combine telemetryAddData and telemetryUpdate into one method for convenience
     private void telemetryShow(String s, boolean isInput) {
         telemetryAddData("DiagnosticsOp", (isInput ? "Input" : "Output"), s);
